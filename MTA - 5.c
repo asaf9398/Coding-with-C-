476eadb84d846e61a139155d1c94b3ea9ecbf9e3
asaf9398 <asaf9398@gmail.com>
2021-12-02 18:03:48 +0200

    Update MTA - 5.c

--- MTA - 5.c
+++ MTA - 5.c
@@ -1,6 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define ARRAY_MAX_SIZE 20
+#define ARRAY_MAX_SIZE 10
 #define STARTING_FROM_ZERO 0
 typedef struct CourseInfo
 {
@@ -13,20 +13,32 @@ int numCoursesInSemesterA();
 int numCoursesInSemesterB();
 void updateArrayFirstSemester(COURSE_INFO array[], int numOfCourses);
 void updateArraySecondSemester(COURSE_INFO array[], int numOfCourses, int whereToStart);
-void sortingArray(COURSE_INFO array[], int whereToStart, int whereToFinish);
+void sortCourses(COURSE_INFO data[], int size);
 int findSmallest(COURSE_INFO array[], int start, int finish);
 
 
 void main()
 {
-	COURSE_INFO array[ARRAY_MAX_SIZE];
+	COURSE_INFO arrayFirstSemester[ARRAY_MAX_SIZE];
+	COURSE_INFO arraySecondSemester[ARRAY_MAX_SIZE];
 	welcome();
 	int numOfCouresesSemesterA = numCoursesInSemesterA();//getting the num of courses on 1st semester
-	updateArrayFirstSemester(array, numOfCouresesSemesterA);//getting the details of courses on 1st semester
+	updateArrayFirstSemester(arrayFirstSemester, numOfCouresesSemesterA);//getting the details of courses on 1st semester
 	int numOfCouresesSemesterB = numCoursesInSemesterB();//getting the num of courses on 2nd semester
-	updateArraySecondSemester(array, numOfCouresesSemesterB, numOfCouresesSemesterA);//getting the details of courses on 2nd semester
-	sortingArray(array,STARTING_FROM_ZERO,numCoursesInSemesterA);//sorting only 1st semester courses
-	sortingArray(array, numCoursesInSemesterA, numCoursesInSemesterB);//sorting only 2nd semester courses
+	updateArraySecondSemester(arraySecondSemester, numOfCouresesSemesterB, numOfCouresesSemesterA);//getting the details of courses on 2nd semester
+	sortingArray(arrayFirstSemester,STARTING_FROM_ZERO,numCoursesInSemesterA);//sorting only 1st semester courses
+	sortingArray(arraySecondSemester, numCoursesInSemesterA, numCoursesInSemesterB);//sorting only 2nd semester courses
+}
+void sortCourses(COURSE_INFO data[], int size)
+{
+	int i, k;
+	for (i = 0; i < size; i++)
+	{
+		for (k = 0; k - 1 - i < size; k++)
+		{
+
+		}
+	}
 }
 /*The following function gets an array, where to search and retrns the index of the
 struct with the smallest course number*/ 
@@ -43,20 +55,7 @@ int findSmallest(COURSE_INFO array[], int start, int finish)
 	}
 	return temp;//returning the index of the smallest course number
 }
-//The following function gets an array and start&stop points and sorting it by the course number
-void sortingArray(COURSE_INFO array[], int whereToStart, int whereToFinish)
-{/*This is a kind of "Bubble Sort", we running on all over the array 1 time with the loop(n cells)
- & using findSmallest function that runs over the array also n times(not the same n every time)
- this is why the efficiency is O(n^2)*/
-	int k;
-	for (k = whereToStart; k < whereToFinish + whereToStart; k++)//running all over the places in the array
-	{
-		int indexSmallest =findSmallest(array, k, whereToFinish);//finding the lowest course number index
-		COURSE_INFO temp = array[k];//saving the array[k] info
-		array[k] = array[indexSmallest];//putting the smallest course number strtuct in array[k] 
-		array[indexSmallest] = temp;//putting the other struct we saved where the smallest was(for not losing any data)
-	}
-}
+
 /*The following function gets an array, how much courses to put in and where to start in the array 
 and using the users input to put the courses data to the array*/
 void updateArraySecondSemester(COURSE_INFO array[], int numOfCourses,int whereToStart)
